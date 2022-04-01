7e4acef464d95aabe4b2fc0eac489d5dd0751083
Wasit Limprasert <wasit7@gmail.com>
2022-04-01 15:07:52 +0700

    add more 3 tables

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,7 +1,19 @@
 #admin.py
 from django.contrib import admin
-from myapp.models import Customer
+from myapp.models import Customer, Item, OrderItem, Invoice
 
 class CustomerAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Customer, CustomerAdmin)
+
+class ItemAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Item, ItemAdmin)
+
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(OrderItem, OrderItemAdmin)
+
+class InvoiceAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Invoice, InvoiceAdmin)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -20,3 +20,22 @@ def update_profile_signal(sender, instance, created, **kwargs):
         Customer.objects.create(user=instance)
         print('update_profile_signal: create a profile')
 
+#SKU (sugle Keeping Unit)
+class Item(models.Model):
+    UNIT_NAME_CHOICE = [ ("ea","each"), ("kg","kilogram"), ("g","gram") ]
+    title = models.CharField(max_length=100)
+    unit = models.CharField(max_length=5, choices=UNIT_NAME_CHOICE, default="ea")
+    unit_price = models.DecimalField(max_digits=5, decimal_places=2) #999.99
+    image=models.ImageField(upload_to='myimages') #from week05
+    description=models.CharField(max_length=100, null=True, blank=True)
+    def __str__(self):
+        return "%s"%(self.title)
+
+class OrderItem(models.Model):
+    item = models.ForeignKey( Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+
+class Invoice(models.Model):
+    customer = models.ForeignKey( Customer, on_delete=models.CASCADE)
+    order_item = models.ForeignKey( OrderItem, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
