a0f03fac874daa2acc2e09d694ee336fbbd8752f
Wasit Limprasert <wasit7@gmail.com>
2022-04-01 15:15:23 +0700

    add features to admin page

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -7,11 +7,13 @@ class CustomerAdmin(admin.ModelAdmin):
 admin.site.register(Customer, CustomerAdmin)
 
 class ItemAdmin(admin.ModelAdmin):
-    pass
+    list_display=['title','unit','unit_price','description']
 admin.site.register(Item, ItemAdmin)
 
 class OrderItemAdmin(admin.ModelAdmin):
-    pass
+    list_display=['item','quantity']
+    list_display_links=['item']
+    list_editable=['quantity']
 admin.site.register(OrderItem, OrderItemAdmin)
 
 class InvoiceAdmin(admin.ModelAdmin):
