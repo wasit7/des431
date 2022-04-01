b8747b5b112dc59c67957730153d72ffc2894a4c
Wasit Limprasert <wasit7@gmail.com>
2022-04-01 14:45:25 +0700

    adding post_save signal

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,12 +1,22 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     fullname = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     postcode = models.CharField(max_length=5)
     email = models.EmailField()
 
     def __str__(self):
-        return "%s"%(self.fullname)
+        return "%s"%(self.user)
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+        print('update_profile_signal: create a profile')
 
